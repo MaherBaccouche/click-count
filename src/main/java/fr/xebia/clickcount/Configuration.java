391c8ec53029a3a1ee8c359a384b2a49e8e573cc
Hamdi KHELIL <khelilhamdi@gmail.com>
2019-01-18 11:43:21 +0100

    Update Configuration.java

--- src/main/java/fr/xebia/clickcount/Configuration.java
+++ src/main/java/fr/xebia/clickcount/Configuration.java
@@ -11,8 +11,24 @@ public class Configuration {
 
     public Configuration() {
         //TODO Fix Configuration to be more flexible 
-        redisHost = "redis";
-        redisPort = 6379;
+        try {
+            
+            redisHost=System.getenv("REDIS_HOST");
+            if (redisHost.isEmpty()) {
+                throw new AssertionError("REDIS_HOST not set as environment variable");
+            } 
+            
+            redisPort=Integer.parseInt(System.getenv("REDIS_PORT"));
+            if (redisHost.isEmpty()) {
+                throw new AssertionError("REDIS_PORT not set as environment variable");
+            } 
+            
+        } catch (SecurityException e) {
+       throw new AssertionError("Security policy doesn't allow access to system environment", e);
+     }
+        
+        //redisHost = "redis";
+        //redisPort = 6379;
         redisConnectionTimeout = 2000;
     }
 }
